d548c78ad1cc89cfb87ce71c15f3ff56e07dd399
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2023-03-25 20:14:19 -0300

    teste sem o eslint

--- public/scripts/Funcoes_God_User.js
+++ public/scripts/Funcoes_God_User.js
@@ -1,6 +1,7 @@
 var formContext;
 window.addEventListener('message',function (event){
     formContext = event.source.Xrm?.Page;
+    console.log(event)
     console.log(formContext);
 }
 );
--- src/pages/Formulario/index.jsx
+++ src/pages/Formulario/index.jsx
@@ -2,16 +2,25 @@ import { Button } from "../../components/Button";
 import { ColumnButton } from "../../components/ColumnButton";
 import { Container } from "./styles";
 
-function Modo_Deus(){
-  console.log(window.Xrm);
+function Modo_Deus2(){
+  chrome.runtime.sendMessage({
+    category: "forms" || '',
+    type: "mododeus",
+  });
 }
 
 export function Formulario(){
+  function Modo_Deus(){
+    chrome.runtime.sendMessage({
+      category: "forms" || '',
+      type: "mododeus",
+    });
+  }
   return(
     <Container>
       <ColumnButton>
         <Button onClick={Modo_Deus}>MODO DEUS</Button>
-        <Button>NOMES LÓGICOS</Button>
+        <Button onClick={Modo_Deus2}>NOMES LÓGICOS</Button>
       </ColumnButton>
       <ColumnButton>
         <Button>LIMPAR NOMES LÓGICOS</Button>
