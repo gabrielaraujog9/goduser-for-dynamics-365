97d2ed15a54d0f1c6ea0b445098e3e5a37d59c53
Victor Leccese <victorleccese@hotmail.com>
2023-03-26 21:29:43 -0300

    conversão de mensagem para json

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,17 +1,16 @@
 chrome.runtime.onMessage.addListener(async function(message,sender,sendResponse ){
-  console.log(message)
-  
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   //if (!tab) return;
 
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: teste,
-    args: [message.toString()] 
+    target: { tabId: tab?.id ?? message.Tab.id },
+    func: Envio,
+    args: [JSON.stringify(message)]
   });
 
   
 });
-function teste(message){
-  window.postMessage(message, '*')
+
+function Envio(message){
+  window.postMessage(JSON.parse(message), '*')
 }
