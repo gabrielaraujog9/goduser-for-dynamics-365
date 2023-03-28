61bdcd0bb1507f10487824bc57cfb596d25ad988
Victor Leccese <victorleccese@hotmail.com>
2023-03-28 13:40:02 -0300

    alterações

--- public/scripts/Funcoes_God_User.js
+++ public/scripts/Funcoes_God_User.js
@@ -1,18 +1,4 @@
-// window.addEventListener('message',async function (event){
-//     try{
-//         if(event?.data?.hasOwnProperty("Mensagem_God_User") && event?.data?.Mensagem_God_User){
-//             console.group("extensão");
-//             console.log(event);
-//             console.groupEnd();
-//         }
-//     }
-//     catch(erro){
-//         console.error(erro);
-//     }
-//   }
-// );
-
-//chrome.runtime.onMessage.addListener(async function(event, sender, sendResponse){
+/* eslint-disable no-undef */
 window.addEventListener('message',async function (mensagem, sender, sendResponse){
     try{
         var event = mensagem.data;
@@ -20,26 +6,27 @@ window.addEventListener('message',async function (mensagem, sender, sendResponse
             if(mensagem.origin !== this.location.origin && this.location.origin !== `${mensagem.origin}.mcas.ms`) return;
             let formContext = mensagem.source.Xrm?.Page;
             let xrm = mensagem.source.Xrm;
+            let form = new FormularioRepository();
             switch(event.Funcao){
-                case Funcoes.AbrirRegistro:
-                    console.log(Funcoes.Texto(Funcoes.AbrirRegistro));
+                case form.Funcoes.AbrirRegistro:
+                    console.log(form.Funcoes.Texto(form.Funcoes.AbrirRegistro));
                     console.log(formContext);
                     break;
-                case Funcoes.CamposAtualizados:
-                    console.log(Funcoes.Texto(Funcoes.CamposAtualizados));
+                case form.Funcoes.CamposAtualizados:
+                    console.log(form.Funcoes.Texto(form.Funcoes.CamposAtualizados));
                     break;
-                case Funcoes.God_Mode:
-                    console.log(Funcoes.Texto(Funcoes.God_Mode));
+                case form.Funcoes.God_Mode:
+                    console.log(form.Funcoes.Texto(form.Funcoes.God_Mode));
                     console.log(formContext);
                     break;
-                case Funcoes.LimparNomesLogicos:
-                    console.log(Funcoes.Texto(Funcoes.LimparNomesLogicos));
+                case form.Funcoes.LimparNomesLogicos:
+                    console.log(form.Funcoes.Texto(form.Funcoes.LimparNomesLogicos));
                     break;
-                case Funcoes.NomesLogicos:
-                    console.log(Funcoes.Texto(Funcoes.NomesLogicos));
+                case form.Funcoes.NomesLogicos:
+                    console.log(form.Funcoes.Texto(form.Funcoes.NomesLogicos));
                     break;
-                case Funcoes.UrlRegistro:
-                    console.log(Funcoes.Texto(Funcoes.UrlRegistro));
+                case form.Funcoes.UrlRegistro:
+                    console.log(form.Funcoes.Texto(form.Funcoes.UrlRegistro));
                     break;
                 default:
                 break;
@@ -52,25 +39,25 @@ window.addEventListener('message',async function (mensagem, sender, sendResponse
     }
 });
 
-const Categorias = {
-    Texto: function(value) {
-        return Object.keys(this).find(key => this[key] === value);
-    },
-    "Form":0,
-    "WebApi":1,
-    "Localizacao":2,
-    "Console":3
-};
+// const Categorias = {
+//     Texto: function(value) {
+//         return Object.keys(this).find(key => this[key] === value);
+//     },
+//     "Form":0,
+//     "WebApi":1,
+//     "Localizacao":2,
+//     "Console":3
+// };
 
-const Funcoes ={
-    Texto: function(value) {
-        return Object.keys(this).find(key => this[key] === value);
-    },
-    "God_Mode":0,
-    "WorkFlows":1,
-    "NomesLogicos":2,
-    "LimparNomesLogicos":3,
-    "AbrirRegistro":4,
-    "UrlRegistro":5,
-    "CamposAtualizados":6
-};
+// const Funcoes ={
+//     Texto: function(value) {
+//         return Object.keys(this).find(key => this[key] === value);
+//     },
+//     "God_Mode":0,
+//     "WorkFlows":1,
+//     "NomesLogicos":2,
+//     "LimparNomesLogicos":3,
+//     "AbrirRegistro":4,
+//     "UrlRegistro":5,
+//     "CamposAtualizados":6
+// };
