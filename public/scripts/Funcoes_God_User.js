a1f85e53b009f43a171cff360c119222a8a75d0b
Victor Leccese <victorleccese@hotmail.com>
2023-03-27 21:05:59 -0300

    validação de origens

--- public/scripts/Funcoes_God_User.js
+++ public/scripts/Funcoes_God_User.js
@@ -1,53 +1,31 @@
-// window.addEventListener('message',async function (event){
-//     try{
-//         if(event?.data?.hasOwnProperty("Mensagem_God_User") && event?.data?.Mensagem_God_User){
-//             console.group("extensão");
-//             console.log(event);
-//             console.groupEnd();
-//         }
-//     }
-//     catch(erro){
-//         console.error(erro);
-//     }
-//   }
-// );
-window.addEventListener('message',async function
-/*chrome.runtime.onMessage.addListener(async function*/(mensagem, sender, sendResponse){
-    try{
-        console.group("extensão");
-        var event = mensagem.data;
-        if(event?.hasOwnProperty("Mensagem_God_User") && event?.Mensagem_God_User){
-            console.log(event);
-            let formContext = mensagem.source.Xrm?.Page;
-            switch(event.Funcao){
-                case Funcoes.AbrirRegistro:
-                    console.log(Funcoes.Texto(Funcoes.AbrirRegistro));
-                    console.log(formContext);
-                    break;
-                case Funcoes.CamposAtualizados:
-                    console.log(Funcoes.Texto(Funcoes.CamposAtualizados));
-                    break;
-                case Funcoes.God_Mode:
-                    console.log(Funcoes.Texto(Funcoes.God_Mode));
-                    console.log(formContext);
-                    break;
-                case Funcoes.LimparNomesLogicos:
-                    console.log(Funcoes.Texto(Funcoes.LimparNomesLogicos));
-                    break;
-                case Funcoes.NomesLogicos:
-                    console.log(Funcoes.Texto(Funcoes.NomesLogicos));
-                    break;
-                case Funcoes.UrlRegistro:
-                    console.log(Funcoes.Texto(Funcoes.UrlRegistro));
-                    break;
-                default:
-                break;
-            }
+window.addEventListener('message',async function(mensagem, sender, sendResponse){
+    var event = mensagem.data;
+    if(event?.hasOwnProperty("Mensagem_God_User") && event?.Mensagem_God_User){
+        if(mensagem.origin !== this.location.origin && this.location.origin !== `${mensagem.origin}.mcas.ms`) return;
+        var formContext = mensagem.source.Xrm?.Page;
+        var xrm = mensagem.source.Xrm;
+        switch(event.Funcao){
+            case Funcoes.AbrirRegistro:
+                console.log(Funcoes.Texto(Funcoes.AbrirRegistro));
+            break;
+            case Funcoes.CamposAtualizados:
+                console.log(Funcoes.Texto(Funcoes.CamposAtualizados));
+            break;
+            case Funcoes.God_Mode:
+                console.log(Funcoes.Texto(Funcoes.God_Mode));
+            break;
+            case Funcoes.LimparNomesLogicos:
+                console.log(Funcoes.Texto(Funcoes.LimparNomesLogicos));
+            break;
+            case Funcoes.NomesLogicos:
+                console.log(Funcoes.Texto(Funcoes.NomesLogicos));
+            break;
+            case Funcoes.UrlRegistro:
+                console.log(Funcoes.Texto(Funcoes.UrlRegistro));
+            break;
+            default:
+            break;
         }
-        console.groupEnd();
-    }
-    catch(erro){
-        console.error(erro);
     }
 });
 
